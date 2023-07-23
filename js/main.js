8f7a88afc6b2938c433a3c6a58aa9c8151bdb396
zebpaa <skvorcov_90@mail.ru>
2023-07-23 20:00:14 +0400

    finished feedback swiper

--- js/main.js
+++ js/main.js
@@ -4,3 +4,18 @@ const menuMobile = document.querySelector('.header__menu-list')
 menuBtn.addEventListener('click', () => {
   menuMobile.classList.toggle('menu--open')
 })
+
+const swiperOne = new Swiper('.feedback__slider', {
+  loop: true,
+
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
+const swiperTwo = new Swiper('.certificates__slider', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});

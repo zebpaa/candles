dcf632c826f9b5ab5a6c7aaf387910082fc68e79
zebpaa <skvorcov_90@mail.ru>
2023-07-27 21:08:11 +0400

    finished the whole project

--- js/main.js
+++ js/main.js
@@ -1,24 +1,24 @@
-const menuBtn = document.querySelector('.menu__btn')
-const menuMobile = document.querySelector('.header__menu-list')
+const menuBtn = document.querySelector(".menu__btn");
+const menuMobile = document.querySelector(".header__menu-list");
 
-menuBtn.addEventListener('click', () => {
-  menuMobile.classList.toggle('menu--open')
-})
+menuBtn.addEventListener("click", () => {
+  menuMobile.classList.toggle("menu--open");
+});
 
-const swiperOne = new Swiper('.feedback__slider', {
+const swiperOne = new Swiper(".feedback__slider", {
   loop: true,
 
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
 });
 
-const swiperTwo = new Swiper('.certificates__slider', {
+const swiperTwo = new Swiper(".certificates__slider", {
   loop: true,
   slidesPerView: 3,
   spaceBetween: 20,
   pagination: {
-    el: '.swiper-pagination',
+    el: ".swiper-pagination",
   },
   breakpoints: {
     640: {
@@ -31,4 +31,23 @@ const swiperTwo = new Swiper('.certificates__slider', {
       slidesPerView: 1,
     },
   },
+});
+
+const accordeon = document.querySelector(".accordeon");
+const accordeonTitles = document.querySelectorAll(".accordeon__title");
+
+accordeonTitles.forEach.call(accordeonTitles, function (accordeonTitle) {
+  accordeonTitle.addEventListener("click", function () {
+    const currentText =
+      accordeonTitle.parentElement.querySelector(".accordeon__text");
+
+    accordeonTitle.classList.toggle("accordeon__title--active");
+    currentText.classList.toggle("accordeon__text--visible");
+
+    if (currentText.classList.contains("accordeon__text--visible")) {
+      currentText.style.maxHeight = currentText.scrollHeight + "px";
+    } else {
+      currentText.style.maxHeight = null;
+    }
+  });
 });
